b33b698e4d1eafd15f8392639831c69ce8de5ee7
cjekel <cjekel@gmail.com>
2020-02-06 15:25:53 -0800

    rename library to pwlftf

---
+++ pwlftf/__init__.py
@@ -0,0 +1,2 @@
+from .pwlftf import PiecewiseLinFitTF  # noqa F401
+from .version import __version__  # noqa F401
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='pwlf',
-    version='1.1.7',
+    name='pwlftf',
+    version='1.0.0',
     author='Charles Jekel',
     author_email='cjekel@gmail.com',
-    packages=['pwlf'],
-    url='https://github.com/cjekel/piecewise_linear_fit_py',
+    packages=['pwlftf'],
+    url='https://github.com/cjekel/piecewise_linear_fit_py_tf',
     license='MIT License',
     description='fit piecewise linear functions to data',
     long_description=open('README.rst').read(),
@@ -16,8 +16,6 @@ setup(
         "scipy >= 1.2.0",
         "pyDOE >= 0.3.8",
         "setuptools >= 38.6.0",
-    ],
-    extras_require={
-        'PiecewiseLinFitTF':  ["tensorflow < 2.0.0"]
-    }
+        "tensorflow < 2.0.0"
+    ]
 )
